140198536e7e7b687e5a0c840083dfa3be3cb913
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-18 00:38:31 +0900

    chore: project setting (eslint, prettier rules)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'eslint-config-prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
@@ -16,12 +17,18 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
+    quotes: ['error', 'single'],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-duplicate-imports': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-unused-vars': 'error',
+    'no-multiple-empty-lines': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
   },
-}
+};
